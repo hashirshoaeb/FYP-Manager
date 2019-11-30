95935a0a8bc1c42c16f292d8daffe1fcfd8c1be3
hashirshoaeb <hashirshoaeb@gmail.com>
2019-11-30 14:54:32 +0500

    http practice

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-var data = require("./module");
+// var data = require("./module");
 
-data(2, 4, (err, response) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    console.log(response);
-    console.log(response.area(3, 4));
-    console.log(response.perimeter(3, 2));
-  }
+// data(2, 4, (err, response) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(response);
+//     console.log(response.area(3, 4));
+//     console.log(response.perimeter(3, 2));
+//   }
+// });
+const http = require("http");
+// console.log(http);
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, response) => {
+  // console.log(req);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at https://${hostname}:${port}`);
 });
