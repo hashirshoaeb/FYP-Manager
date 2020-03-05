7355e60b501ceedfa4c648a64077b4feb638ab3b
khizarkhizar <khizarrana756@gmail.com>
2020-03-06 01:04:42 +0500

    adding component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import Signin from "./components/signin";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="primary">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </button>
-      </header>
-    </div>
-  );
+  return <Signin></Signin>;
 }
 
 export default App;
---
+++ src/components/signin.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Text Signin</div>;
+  }
+}
+
+export default Signin;
