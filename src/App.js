4a0e260071c73de594b3a72868d91d61e2a709ab
hashirshoaeb <hashirshoaeb@gmail.com>
2020-03-15 03:20:43 +0500

    working on nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 // import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import MainBody from "./components/MainBody";
 import Project from "./components/Project";
 import Signin from "./components/Signin";
@@ -12,9 +13,10 @@ import About from "./components/About";
 function App() {
   return (
     <div>
-      <MainBody></MainBody>  
-      <About></About>  
-      <Project></Project>    
+      <Navbar></Navbar>
+      <MainBody></MainBody>
+      <About></About>
+      <Project></Project>
       <Working></Working>
       <Signin></Signin>
       <Footer></Footer>
