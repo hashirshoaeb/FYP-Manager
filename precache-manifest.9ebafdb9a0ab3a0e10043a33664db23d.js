1abd6cc03fd5a5a9d8c157e29252a595deb8b3b6
hashirshoaeb <hashirshoaeb@gmail.com>
2020-03-16 04:01:37 +0500

    Updates

---
+++ precache-manifest.9ebafdb9a0ab3a0e10043a33664db23d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cec82b861078bfc71fd5d609c7bf42",
+    "url": "/FYP-Manager/index.html"
+  },
+  {
+    "revision": "d45d497030367c39b568",
+    "url": "/FYP-Manager/static/css/2.3db4eb7d.chunk.css"
+  },
+  {
+    "revision": "d45d497030367c39b568",
+    "url": "/FYP-Manager/static/js/2.908f5933.chunk.js"
+  },
+  {
+    "revision": "2ad48c5609cffb0658c18b89ee7b9e01",
+    "url": "/FYP-Manager/static/js/2.908f5933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4047f423f295c874b0",
+    "url": "/FYP-Manager/static/js/main.16149e44.chunk.js"
+  },
+  {
+    "revision": "18f5ad9705f503668378",
+    "url": "/FYP-Manager/static/js/runtime-main.d28d70ad.js"
+  },
+  {
+    "revision": "b11f6a823db102782d232a685e93419a",
+    "url": "/FYP-Manager/static/media/collaboration.b11f6a82.svg"
+  },
+  {
+    "revision": "8f5fa3ff26fce619a040a92d25e41173",
+    "url": "/FYP-Manager/static/media/process.8f5fa3ff.svg"
+  },
+  {
+    "revision": "8402044c8980b1283c01810fd720461a",
+    "url": "/FYP-Manager/static/media/scrum_board.8402044c.svg"
+  },
+  {
+    "revision": "2e353da7ae178927e43f636950e79d27",
+    "url": "/FYP-Manager/static/media/version_control.2e353da7.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/FYP-Manager/precache-manifest.9ebafdb9a0ab3a0e10043a33664db23d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FYP-Manager/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
