c0cd05b65673bca912e79f06fd4d97b6ff38c821
khizarkhizar <khizarrana756@gmail.com>
2020-03-17 02:19:28 +0500

    pro

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,26 +11,28 @@ class ProjectCard extends Component {
   render() {
     return (
       <div className="card shadow my-5 mx-5">
-        <div className="card-body p-4">
-          <h4 className="card-title">{this.state.data.Fyp_title}</h4>
-          <hr />
-          <p className="lead">
-            <b>Supervisor: </b>{" "}
-            <div className="btn btn-outline-dark">
-              {" "}
-              {this.state.data.Supervisor}
-            </div>
-          </p>
-          <h5 className="">Desctiption:</h5>
-          <p className="card-text">{this.state.data.Fyp_desc}</p>
-          <hr />
-          <h5 className="h5">Tags:</h5>
-          {this.state.data.Fyp_tag.map(t => (
-            <div key={t} className="btn btn-outline-dark  m-2">
-              {t}
-            </div>
-          ))}
-        </div>
+        <a className="btn" href="#Login">
+          <div className="card-body p-4">
+            <h4 className="card-title">{this.state.data.Fyp_title}</h4>
+            <hr />
+            <p className="lead">
+              <b>Supervisor: </b>{" "}
+              <div className="btn btn-outline-dark">
+                {" "}
+                {this.state.data.Supervisor}
+              </div>
+            </p>
+            <h5 className="">Desctiption:</h5>
+            <p className="card-text">{this.state.data.Fyp_desc}</p>
+            <hr />
+            <h5 className="h5">Tags:</h5>
+            {this.state.data.Fyp_tag.map(t => (
+              <div key={t} className="btn btn-outline-dark  m-2">
+                {t}
+              </div>
+            ))}
+          </div>
+        </a>
       </div>
     );
   }
