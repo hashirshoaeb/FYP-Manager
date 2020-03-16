1b216b63f44fa26498ad6750c5d65591669b80b0
hashirshoaeb <hashirshoaeb@gmail.com>
2020-03-17 01:32:54 +0500

    carousel removed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,37 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import ProjectCard from "./ProjectCard";
 import Data from "../projects.json";
-import ItemsCarousel from "react-items-carousel";
 
 export default () => {
   const state = { data: Data.Fyp };
-
-  const [activeItemIndex, setActiveItemIndex] = useState(0);
-  const chevronWidth = 40;
+  const numberOfCards = 3
   return (
     <div className="jumbotron my-5">
       <div className="container-fluid">
         {" "}
         <h1 className="display-4 text-center"> Recent Projects</h1>{" "}
-        <div style={{ padding: `0 ${chevronWidth}px` }}>
-          <ItemsCarousel
-            requestToChangeActive={setActiveItemIndex}
-            activeItemIndex={activeItemIndex}
-            numberOfCards={3}
-            leftChevron={<div className="btn btn-outline-dark">{"<"}</div>}
-            rightChevron={<div className="btn btn-outline-dark">{">"}</div>}
-            outsideChevron
-            chevronWidth={chevronWidth}
-          >
-            {state.data.map(d => (
+        <div className="row">
+          {state.data.map(d => (
+            d.id <= numberOfCards &&
+            <div className="col-md-4">
               <ProjectCard key={d.id} data={d}>
                 {" "}
               </ProjectCard>
-            ))}
-          </ItemsCarousel>
-        </div>{" "}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
- 
