8d3c570a1a28607dc422666b9f002eebe7131866
hashirshoaeb <hashirshoaeb@gmail.com>
2020-03-21 12:45:59 +0500

    changes in ui

--- src/components/webapp-components/UserProfile.jsx
+++ src/components/webapp-components/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import data from "../../projects.json";
+import { Link } from "react-router-dom";
 class UserProfile extends Component {
   constructor(props) {
     super(props);
@@ -7,65 +8,86 @@ class UserProfile extends Component {
   }
   render() {
     return (
-      <div className="row justify-content-center">
-        <div className="col-md-3  my-5">
-          <div className="card shadow">
-            <div className="card-body mx-auto">
-              <img className="img-responsive" alt="" />
-
-              <div className="lead">
-                <div className="display-4 text-center ">khizar</div>
-                <div className="text-center">Supervisor</div>
-              </div>
-
-              <div className="my-3">
-                <button type="button" className="btn btn-success btn-sm mx-2">
-                  Follow
-                </button>
-                <button type="button" className="btn btn-danger btn-sm mx-2">
-                  {" "}
-                  Message
-                </button>
-              </div>
-            </div>
-
-            <div className="card-body text-center mx-1">
-              <table className="table table-hover">
-                <tbody>
-                  <tr>
-                    <td>
-                      {" "}
-                      <a href="/">Account Settings </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      {" "}
-                      <a href="/">Tasks </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      {" "}
-                      <a href="/">help </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+      <div className="container">
+        <div className="row">
+          <ProfileCard />
+          <div className="col-md-8 my-5">
+            <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <Link to="/profile" class="nav-link">
+                  Projects
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/profile?tab=requests" class="nav-link">
+                  Requests
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/profile?tab=link" class="nav-link">
+                  Link
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/profile?tab=disable" class="nav-link">
+                  Disabled
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="col-md-6    my-5">
-          <div className="card shadow">
-            <div className="card-body mx-auto">
-              My All Projects
-              {this.state.data.map(d => (
-                <div className="card my-3 p-1">
-                  <h5 className="card_title">{d.Fyp_title}</h5>
-                  <div className="btn btn-outline-danger">Assign to Umar</div>
-                </div>
-              ))}
+      </div>
+    );
+  }
+}
+
+class ProfileCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="col-md-4  my-5">
+        <div className="card shadow">
+          <div className="card-body mx-auto">
+            <img className="img-responsive" alt="" />
+            <div className="lead">
+              <div className="display-4 text-center ">khizar</div>
+              <div className="text-center">Supervisor</div>
             </div>
+            <div className="my-3">
+              <button type="button" className="btn btn-success btn-sm mx-2">
+                Follow
+              </button>
+              <button type="button" className="btn btn-danger btn-sm mx-2">
+                Message
+              </button>
+            </div>
+          </div>
+          <div className="card-body text-center mx-1">
+            <table className="table table-hover">
+              <tbody>
+                <tr>
+                  <td>
+                    {" "}
+                    <a href="/">Account Settings </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <a href="/">Tasks </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <a href="/">help </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
